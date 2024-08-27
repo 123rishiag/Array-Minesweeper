f0ad71f4d4d7da96acc033f46bf0b6fe1390d61c
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-27 22:01:09 +0530

    Positioned Cell on Board

--- Array-Minesweeper/header/Gameplay/Cell/CellView.h
+++ Array-Minesweeper/header/Gameplay/Cell/CellView.h
@@ -14,17 +14,22 @@ namespace Gameplay
             const int tile_size = 32;
             const int slice_count = 12;
 
+            const float cell_top_offset = 274.f;
+            const float cell_left_offset = 583.f;
+
             UI::UIElement::ButtonView* cell_button;
             CellController* cell_controller;
 
             void initializeButtonImage(float width, float height);
             void setCellTexture();
 
+            sf::Vector2f getCellScreenPosition() const;
+
         public:
             CellView(CellController* controller);
             ~CellView();
 
-            void initialize();
+            void initialize(float width, float height);
             void update();
             void render();
         };

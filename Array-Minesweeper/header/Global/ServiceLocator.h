af78d0fe623180e1c136a1c278598b4c5aa2a79a
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-27 17:21:44 +0530

    Created Board Controller and Board Service

--- Array-Minesweeper/header/Global/ServiceLocator.h
+++ Array-Minesweeper/header/Global/ServiceLocator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../../header/Graphics/GraphicService.h"
 #include "../../header/Event/EventService.h"
+#include "../../header/Gameplay/Board/BoardService.h"
 #include "../../header/UI/UIService.h"
 #include "../../header/Sound/SoundService.h"
 
@@ -9,8 +10,9 @@ namespace Global
     class ServiceLocator
     {
     private:
-        Event::EventService* event_service;
         Graphics::GraphicService* graphic_service;
+        Event::EventService* event_service;
+        Gameplay::Board::BoardService* board_service;
         Sound::SoundService* sound_service;
         UI::UIService* ui_service;
 
@@ -27,8 +29,9 @@ namespace Global
         void update();
         void render();
 
-        Event::EventService* getEventService();
         Graphics::GraphicService* getGraphicService();
+        Event::EventService* getEventService();
+        Gameplay::Board::BoardService* getBoardService();
         Sound::SoundService* getSoundService();
         UI::UIService* getUIService();
         void deleteServiceLocator();

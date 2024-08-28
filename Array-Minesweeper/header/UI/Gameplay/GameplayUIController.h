b7e919bf980f85b377612392d195038b37431354
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-28 22:10:45 +0530

    Fixed GameplayUIController

--- Array-Minesweeper/header/UI/Gameplay/GameplayUIController.h
+++ Array-Minesweeper/header/UI/Gameplay/GameplayUIController.h
@@ -11,21 +11,25 @@ namespace UI
         class GameplayUIController : public Interface::IUIController
         {
             const int font_size = 110;
-            const sf::Color text_color = sf::Color::Red;
-
-            const float time_text_top_offset = 65.f;
-            const float time_text_left_offset = 1090.f;
-            UIElement::TextView* time_text;
 
             const float mine_text_top_offset = 65.f;
             const float mine_text_left_offset = 660.f;
-            UIElement::TextView* mine_text;
+
+            const float time_text_top_offset = 65.f;
+            const float time_text_left_offset = 1090.f;
 
             const float restart_button_top_offset = 100.f;
             const float restart_button_left_offset = 920.f;
+
             const float button_height = 80.f;
             const float button_width = 80.f;
+
             const int tile_height = 32;
+
+            const sf::Color text_color = sf::Color::Red;
+
+            UIElement::TextView* mine_text;
+            UIElement::TextView* time_text;
             UIElement::ButtonView* restart_button;
 
             void createButton();
@@ -36,8 +40,8 @@ namespace UI
             void initializeTimeText();
             void updateMineText();
             void updateTimeText();
-            void registerButtonCallback();
             void restartButtonCallback();
+            void registerButtonCallback();
 
             void destroy();
 

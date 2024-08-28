6a7245e95d64d01b2442a42c25917572776a6900
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-28 21:25:57 +0530

    Implemented Reset Button

--- Array-Minesweeper/header/UI/Gameplay/GameplayUIController.h
+++ Array-Minesweeper/header/UI/Gameplay/GameplayUIController.h
@@ -14,20 +14,31 @@ namespace UI
 
             const float time_text_top_offset = 65.f;
             const float time_text_left_offset = 1090.f;
-
             UIElement::TextView* time_text;
 
             const float mine_text_top_offset = 65.f;
             const float mine_text_left_offset = 660.f;
-
             UIElement::TextView* mine_text;
 
+            const float restart_button_top_offset = 100.f;
+            const float restart_button_left_offset = 920.f;
+            const float button_height = 80.f;
+            const float button_width = 80.f;
+            const int tile_height = 32;
+            UIElement::ButtonView* restart_button;
+
+            void createButton();
             void createTexts();
+            void initializeButton();
             void initializeTexts();
             void initializeMineText();
             void initializeTimeText();
             void updateMineText();
             void updateTimeText();
+            void registerButtonCallback();
+            void restartButtonCallback();
+
+            void destroy();
 
         public:
             GameplayUIController();

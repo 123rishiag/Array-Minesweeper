334bd4ccf555dbd85c278d85b89a2c2d8614ce5c
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-29 18:01:32 +0530

    Fixed some issue in game

--- Array-Minesweeper/source/Gameplay/GameplayController.cpp
+++ Array-Minesweeper/source/Gameplay/GameplayController.cpp
@@ -17,10 +17,7 @@ namespace Gameplay
 		board_service = ServiceLocator::getInstance()->getBoardService();
 	}
 
-	GameplayController::~GameplayController()
-	{
-		delete(board_service);
-	}
+	GameplayController::~GameplayController() {	}
 
 	void GameplayController::initialize()
 	{

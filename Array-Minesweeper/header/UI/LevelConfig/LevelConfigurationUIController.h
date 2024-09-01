8de218343070b6ce414d8647659d926f37b2e442
Rishabh Garg <narishabh.garg@gmail.com>
2024-09-01 09:38:08 +0530

    Optimized and Included some case check in Level Config

--- Array-Minesweeper/header/UI/LevelConfig/LevelConfigurationUIController.h
+++ Array-Minesweeper/header/UI/LevelConfig/LevelConfigurationUIController.h
@@ -1,7 +1,10 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 #include "../../header/UI/Interface/IUIController.h"
 #include "../../header/UI/UIElement/TextView.h"
 #include "../../header/UI/UIElement/ButtonView.h"
+#include "../../header/UI/UIElement/ImageView.h"
+#include <vector>
 
 namespace UI
 {
@@ -10,35 +13,36 @@ namespace UI
 		class LevelConfigurationUIController : public Interface::IUIController
 		{
 		private:
-			// Constants for UI layout
-			const float button_width = 200.f;
-			const float button_height = 60.f;
-
-			const float increment_button_width = 50.f;
-			const float increment_button_height = 50.f;
-
-			const float start_button_y_position = 600.f;
+			const float button_width = 400.f;
+			const float button_height = 140.f;
+			const float increment_button_width = 200.f;
+			const float increment_button_height = 200.f;
+			const float start_button_x_position = 100.f;
+			const float start_button_y_position = 900.f;
 			const float background_alpha = 85.f;
+			const int font_size = 100;
+			const float input_field_y_start = 150.f;
+			const float input_field_spacing = 200.f;
+			const int min_rows_columns = 5;
+			const int max_rows_columns = 32;
+			const int min_mines_factor = 12;
+			const int max_mines_factor = 10;
+			const int num_fields = 3;
+			const float text_base_x = 1100.f;
+			const float increment_button_x = 500.f;
+			const float decrement_button_x = increment_button_x + increment_button_width;
+			const sf::Color text_color = sf::Color::White;
 
-			const int font_size = 30; // Font size for input labels and current values
-			const int validation_font_size = 24; // Smaller font size for validation messages
+			// Position for validation message
+			sf::Vector2f validation_message_position = sf::Vector2f(600.f, input_field_y_start + (input_field_spacing / 3) + num_fields * input_field_spacing);
 
-			const float input_field_y_start = 200.f; // Starting Y position for input fields
-			const float input_field_spacing = 100.f; // Spacing between input fields
+			// Background image
+			UIElement::ImageView* background_image;
 
-			// Text views to display the current values of rows, columns, mines, and validation message
-			UIElement::TextView* rows_text;
-			UIElement::TextView* columns_text;
-			UIElement::TextView* mines_text;
+			// UI Elements
+			std::vector<UIElement::TextView*> text_views;
 			UIElement::TextView* validation_message;
-
-			// Buttons to increase/decrease rows, columns, and mines
-			UIElement::ButtonView* rows_increase_button;
-			UIElement::ButtonView* rows_decrease_button;
-			UIElement::ButtonView* columns_increase_button;
-			UIElement::ButtonView* columns_decrease_button;
-			UIElement::ButtonView* mines_increase_button;
-			UIElement::ButtonView* mines_decrease_button;
+			std::vector<UIElement::ButtonView*> buttons;
 			UIElement::ButtonView* start_button;
 
 			// Current values for rows, columns, and mines
@@ -46,12 +50,14 @@ namespace UI
 			int current_columns;
 			int current_mines;
 
+			void createImage();
 			void createButtons();
 			void createTextFields();
+			void initializeBackgroundImage();
 			void initializeTextFields();
 			void initializeButtons();
 			void registerButtonCallbacks();
-			void validateInputs();
+			bool validateInputs();
 			void updateDisplayedValues();
 			void startGameButtonCallback();
 			void increaseRows();
@@ -60,6 +66,7 @@ namespace UI
 			void decreaseColumns();
 			void increaseMines();
 			void decreaseMines();
+			void destroy();
 
 		public:
 			LevelConfigurationUIController();
@@ -69,7 +76,6 @@ namespace UI
 			void update() override;
 			void render() override;
 			void show() override;
-			void destroy();
 		};
 	}
 }
--- Array-Minesweeper/source/LevelConfig/LevelConfigurationUIController.cpp
+++ Array-Minesweeper/source/LevelConfig/LevelConfigurationUIController.cpp
@@ -18,6 +18,7 @@ namespace UI
 
 		LevelConfigurationUIController::LevelConfigurationUIController()
 		{
+			createImage();
 			createButtons();
 			createTextFields();
 		}
@@ -29,30 +30,44 @@ namespace UI
 
 		void LevelConfigurationUIController::initialize()
 		{
+			initializeBackgroundImage();
 			initializeTextFields();
 			initializeButtons();
 			registerButtonCallbacks();
 		}
 
+		void LevelConfigurationUIController::createImage()
+		{
+			background_image = new ImageView();
+		}
+
 		void LevelConfigurationUIController::createButtons()
 		{
-			// Create buttons for increasing/decreasing rows, columns, and mines
-			rows_increase_button = new ButtonView();
-			rows_decrease_button = new ButtonView();
-			columns_increase_button = new ButtonView();
-			columns_decrease_button = new ButtonView();
-			mines_increase_button = new ButtonView();
-			mines_decrease_button = new ButtonView();
+			for (int i = 0; i < num_fields; i++)
+			{
+				auto increase_button = new ButtonView();
+				auto decrease_button = new ButtonView();
+				buttons.push_back(increase_button);
+				buttons.push_back(decrease_button);
+			}
 			start_button = new ButtonView();
 		}
 
 		void LevelConfigurationUIController::createTextFields()
 		{
-			// Create text views to display current values and validation message
-			rows_text = new TextView();
-			columns_text = new TextView();
-			mines_text = new TextView();
+			for (int i = 0; i < num_fields; i++)
+			{
+				auto text_view = new TextView();
+				text_views.push_back(text_view);
+			}
 			validation_message = new TextView();
+		}
+
+		void LevelConfigurationUIController::initializeBackgroundImage()
+		{
+			sf::RenderWindow* game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+			background_image->initialize(Config::background_texture_path, game_window->getSize().x, game_window->getSize().y, sf::Vector2f(0, 0));
+			background_image->setImageAlpha(background_alpha);
 		}
 
 		void LevelConfigurationUIController::initializeTextFields()
@@ -61,116 +76,126 @@ namespace UI
 			current_columns = ServiceLocator::getInstance()->getBoardService()->getSelectedColumnsCount();
 			current_mines = ServiceLocator::getInstance()->getBoardService()->getSelectedMinesCount();
 
-			// Initialize text fields with current values
-			rows_text->initialize("Rows: " + std::to_string(current_rows), sf::Vector2f(100, input_field_y_start), FontType::BUBBLE_BOBBLE, font_size, sf::Color::White);
-			rows_text->setTextCentreAligned();
+			for (int i = 0; i < num_fields; i++)
+			{
+				float y_position = input_field_y_start + i * input_field_spacing;
+				sf::Vector2f text_position(text_base_x, y_position);
+				text_views[i]->initialize("", text_position, FontType::BUBBLE_BOBBLE, font_size, text_color);
+			}
 
-			columns_text->initialize("Columns: " + std::to_string(current_columns), sf::Vector2f(100, input_field_y_start + input_field_spacing), FontType::BUBBLE_BOBBLE, font_size, sf::Color::White);
-			columns_text->setTextCentreAligned();
-
-			mines_text->initialize("Mines: " + std::to_string(current_mines), sf::Vector2f(100, input_field_y_start + 2 * input_field_spacing), FontType::BUBBLE_BOBBLE, font_size, sf::Color::White);
-			mines_text->setTextCentreAligned();
-
-			validation_message->initialize("", sf::Vector2f(100, input_field_y_start + 3 * input_field_spacing), FontType::ROBOTO, validation_font_size, sf::Color::Red);
-			validation_message->setTextCentreAligned();
+			validation_message->initialize("", validation_message_position, FontType::ROBOTO, font_size / 2, text_color);
+			updateDisplayedValues();
 		}
 
 		void LevelConfigurationUIController::initializeButtons()
 		{
-			// Initialize buttons
-			rows_increase_button->initialize("+", Config::increment_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(300, input_field_y_start));
-			rows_decrease_button->initialize("-", Config::decrement_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(350, input_field_y_start));
+			for (int i = 0; i < num_fields; i++)
+			{
+				float y_position = input_field_y_start + i * input_field_spacing;
+				buttons[2 * i]->initialize("+", Config::increment_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(increment_button_x, y_position));
+				buttons[2 * i + 1]->initialize("-", Config::decrement_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(decrement_button_x, y_position));
+			}
 
-			columns_increase_button->initialize("+", Config::increment_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(300, input_field_y_start + input_field_spacing));
-			columns_decrease_button->initialize("-", Config::decrement_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(350, input_field_y_start + input_field_spacing));
-
-			mines_increase_button->initialize("+", Config::increment_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(300, input_field_y_start + 2 * input_field_spacing));
-			mines_decrease_button->initialize("-", Config::decrement_button_texture_path, increment_button_width, increment_button_height, sf::Vector2f(350, input_field_y_start + 2 * input_field_spacing));
-
-			start_button->initialize("Start Game", Config::play_button_texture_path, button_width, button_height, sf::Vector2f(100, start_button_y_position));
+			start_button->initialize("Start Game", Config::play_button_texture_path, button_width, button_height, sf::Vector2f(start_button_x_position, start_button_y_position));
 			start_button->setCentreAlinged();
 		}
 
 		void LevelConfigurationUIController::registerButtonCallbacks()
 		{
-			rows_increase_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::increaseRows, this));
-			rows_decrease_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::decreaseRows, this));
-			columns_increase_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::increaseColumns, this));
-			columns_decrease_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::decreaseColumns, this));
-			mines_increase_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::increaseMines, this));
-			mines_decrease_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::decreaseMines, this));
+			buttons[0]->registerCallbackFunction(std::bind(&LevelConfigurationUIController::increaseRows, this));
+			buttons[1]->registerCallbackFunction(std::bind(&LevelConfigurationUIController::decreaseRows, this));
+			buttons[2]->registerCallbackFunction(std::bind(&LevelConfigurationUIController::increaseColumns, this));
+			buttons[3]->registerCallbackFunction(std::bind(&LevelConfigurationUIController::decreaseColumns, this));
+			buttons[4]->registerCallbackFunction(std::bind(&LevelConfigurationUIController::increaseMines, this));
+			buttons[5]->registerCallbackFunction(std::bind(&LevelConfigurationUIController::decreaseMines, this));
 			start_button->registerCallbackFunction(std::bind(&LevelConfigurationUIController::startGameButtonCallback, this));
 		}
 
 		void LevelConfigurationUIController::startGameButtonCallback()
 		{
-			validateInputs();
-		}
-
-		void LevelConfigurationUIController::validateInputs()
-		{
-			if (current_rows < 5 || current_rows > 32 || current_columns < 5 || current_columns > 32)
-			{
-				validation_message->setText("Rows and Columns must be between 5 and 32.");
-				return;
-			}
-
-			int minMines = (current_rows * current_columns) / 12;
-			int maxMines = (current_rows * current_columns) / 10;
-
-			if (current_mines < minMines || current_mines > maxMines)
-			{
-				validation_message->setText("Mines must be between " + std::to_string(minMines) + " and " + std::to_string(maxMines) + ".");
-			}
-			else
-			{
-				validation_message->setText("");  // Clear any previous error messages
-
-				// Proceed to start game
+			bool isValidInput = validateInputs();
+			if (isValidInput) {
 				ServiceLocator::getInstance()->getBoardService()->resetBoard(current_rows, current_columns, current_mines);
 				GameService::setGameState(GameState::GAMEPLAY);
 			}
 		}
 
+		bool LevelConfigurationUIController::validateInputs()
+		{
+			bool isValidInput = true;
+			int minMines = (current_rows * current_columns) / min_mines_factor;
+			int maxMines = (current_rows * current_columns) / max_mines_factor;
+
+			if (current_mines < minMines)
+			{
+				current_mines = minMines;
+				validation_message->setText("Mines adjusted to minimum: " + std::to_string(minMines));
+				isValidInput = false;
+			}
+			else if (current_mines > maxMines)
+			{
+				current_mines = maxMines;
+				validation_message->setText("Mines adjusted to maximum: " + std::to_string(maxMines));
+				isValidInput = false;
+			}
+			else
+			{
+				validation_message->setText("");
+			}
+
+			if (current_rows < min_rows_columns || current_rows > max_rows_columns || current_columns < min_rows_columns || current_columns > max_rows_columns)
+			{
+				validation_message->setText("Rows and Columns must be between " + std::to_string(min_rows_columns) + " and " + std::to_string(max_rows_columns) + ".");
+				isValidInput = false;
+			}
+
+			updateDisplayedValues();
+			return isValidInput;
+		}
+
 		void LevelConfigurationUIController::increaseRows()
 		{
-			if (current_rows < 32)
+			if (current_rows < max_rows_columns)
 			{
 				current_rows++;
+				validateInputs();
 				updateDisplayedValues();
 			}
 		}
 
 		void LevelConfigurationUIController::decreaseRows()
 		{
-			if (current_rows > 5)
+			if (current_rows > min_rows_columns)
 			{
 				current_rows--;
+				validateInputs();
 				updateDisplayedValues();
 			}
 		}
 
 		void LevelConfigurationUIController::increaseColumns()
 		{
-			if (current_columns < 32)
+			if (current_columns < max_rows_columns)
 			{
 				current_columns++;
+				validateInputs();
 				updateDisplayedValues();
 			}
 		}
 
 		void LevelConfigurationUIController::decreaseColumns()
 		{
-			if (current_columns > 5)
+			if (current_columns > min_rows_columns)
 			{
 				current_columns--;
+				validateInputs();
 				updateDisplayedValues();
 			}
 		}
 
 		void LevelConfigurationUIController::increaseMines()
 		{
-			if (current_mines < (current_rows * current_columns) / 10)
+			if (current_mines < (current_rows * current_columns) / max_mines_factor)
 			{
 				current_mines++;
 				updateDisplayedValues();
@@ -179,7 +204,7 @@ namespace UI
 
 		void LevelConfigurationUIController::decreaseMines()
 		{
-			if (current_mines > (current_rows * current_columns) / 12)
+			if (current_mines > (current_rows * current_columns) / min_mines_factor)
 			{
 				current_mines--;
 				updateDisplayedValues();
@@ -188,69 +213,69 @@ namespace UI
 
 		void LevelConfigurationUIController::updateDisplayedValues()
 		{
-			rows_text->setText("Rows: " + std::to_string(current_rows));
-			columns_text->setText("Columns: " + std::to_string(current_columns));
-			mines_text->setText("Mines: " + std::to_string(current_mines));
+			text_views[0]->setText("Rows: " + std::to_string(current_rows));
+			text_views[1]->setText("Columns: " + std::to_string(current_columns));
+			text_views[2]->setText("Mines: " + std::to_string(current_mines));
 		}
 
 		void LevelConfigurationUIController::update()
 		{
+			background_image->update();
 			start_button->update();
-			rows_increase_button->update();
-			rows_decrease_button->update();
-			columns_increase_button->update();
-			columns_decrease_button->update();
-			mines_increase_button->update();
-			mines_decrease_button->update();
-			rows_text->update();
-			columns_text->update();
-			mines_text->update();
+			for (auto& button : buttons)
+			{
+				button->update();
+			}
+			for (auto& text_view : text_views)
+			{
+				text_view->update();
+			}
 			validation_message->update();
 		}
 
 		void LevelConfigurationUIController::render()
 		{
+			background_image->render();
 			start_button->render();
-			rows_increase_button->render();
-			rows_decrease_button->render();
-			columns_increase_button->render();
-			columns_decrease_button->render();
-			mines_increase_button->render();
-			mines_decrease_button->render();
-			rows_text->render();
-			columns_text->render();
-			mines_text->render();
+			for (auto& button : buttons)
+			{
+				button->render();
+			}
+			for (auto& text_view : text_views)
+			{
+				text_view->render();
+			}
 			validation_message->render();
 		}
 
 		void LevelConfigurationUIController::show()
 		{
+			background_image->show();
 			start_button->show();
-			rows_increase_button->show();
-			rows_decrease_button->show();
-			columns_increase_button->show();
-			columns_decrease_button->show();
-			mines_increase_button->show();
-			mines_decrease_button->show();
-			rows_text->show();
-			columns_text->show();
-			mines_text->show();
+			for (auto& button : buttons)
+			{
+				button->show();
+			}
+			for (auto& text_view : text_views)
+			{
+				text_view->show();
+			}
 			validation_message->show();
 		}
 
 		void LevelConfigurationUIController::destroy()
 		{
-			delete start_button;
-			delete rows_increase_button;
-			delete rows_decrease_button;
-			delete columns_increase_button;
-			delete columns_decrease_button;
-			delete mines_increase_button;
-			delete mines_decrease_button;
-			delete rows_text;
-			delete columns_text;
-			delete mines_text;
-			delete validation_message;
+			for (auto& button : buttons)
+			{
+				delete(button);
+			}
+			for (auto& text_view : text_views)
+			{
+				delete(text_view);
+			}
+			delete(validation_message);
+			delete(start_button);
+			delete(background_image);
 		}
 	}
 }
